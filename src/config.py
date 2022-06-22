a6ff2e07578a44db230180cbc960008ff1002279
KIMATA Tetsuya <kimata@green-rabbit.net>
2022-06-22 10:53:03 +0900

    doc: 説明方法を変更

--- src/config.py
+++ src/config.py
@@ -4,8 +4,7 @@ PORT = "/dev/ttyAMA0"
 # Fluentd のアドレス
 FLUENT_HOST = "columbia.green-rabbit.net"
 
-# 中継器に記録される「INFO; Receive the report form addr = 0xXXXX」
-# の「0xXXXX」を名前に変換するテーブル
+# sniffer.py が収集するデータの dev_id を名前に変換するテーブル
 DEV_MAP = {
     # 4219: "書斎エアコン",
     0xDC17: "リビングエアコン",
