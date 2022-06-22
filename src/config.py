e1738182f391937442e3efc03e11f9b540528472
KIMATA Tetsuya <kimata@green-rabbit.net>
2022-06-22 10:36:02 +0900

    doc: コメントを追記

--- src/config.py
+++ src/config.py
@@ -4,7 +4,9 @@ PORT = "/dev/ttyAMA0"
 # Fluentd のアドレス
 FLUENT_HOST = "columbia.green-rabbit.net"
 
+# 中継器に記録される「INFO; Receive the report form addr = 0xXXXX」
+# の「0xXXXX」を名前に変換するテーブル
 DEV_MAP = {
     # 4219: "書斎エアコン",
-    56343: "リビングエアコン",
+    0xDC17: "リビングエアコン",
 }
